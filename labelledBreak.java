5609da68cedb5cf55ed8ce9912d78cb1651ae524
chinmayi-m-n <1cg21cs023@cittumkur.org>
2025-03-10 14:12:51 +0530

    labelledBreak

---
+++ labelledBreak.java
@@ -0,0 +1,12 @@
+public class labelledBreak {
+    public static void main(String[] args) {
+        label_name:
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                if(i==3)break label_name;
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
