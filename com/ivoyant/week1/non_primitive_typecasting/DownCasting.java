0ff8967851f868769ac4415e20adda4193290af6
chinmayi-m-n <1cg21cs023@cittumkur.org>
2025-03-11 23:22:24 +0530

    downcasting

---
+++ com/ivoyant/week1/non_primitive_typecasting/DownCasting.java
@@ -0,0 +1,24 @@
+package com.ivoyant.week1.non_primitive_typecasting;
+
+class Computer{
+    public void code(){
+        System.out.println("coding.....");
+    }
+}
+class Laptop extends Computer{
+    public void code(){
+        System.out.println("coding in laptop...");
+    }
+}
+class Desktop extends Computer{
+    public void code(){
+        System.out.println("coding in desktop.....");
+    }
+}
+class DownCasting{
+    public static void main(String[] args) {
+        //moving from parent towards child
+        Laptop obj1=(Laptop)new Computer();
+        obj1.code();
+    }
+}
