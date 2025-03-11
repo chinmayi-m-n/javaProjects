b6f0e4d60c2cbc472ad4a5224957a22aed087ea4
chinmayi-m-n <1cg21cs023@cittumkur.org>
2025-03-11 23:26:11 +0530

    primitive typecasting

---
+++ com/ivoyant/week1/primitive_typecasting/ExplicitTypeCasting.java
@@ -0,0 +1,13 @@
+package com.ivoyant.week1.primitive_typecasting;
+
+public class ExplicitTypeCasting {
+    public static void main(String[] args) {
+        //explicit typecasting is to be made explicitly when u try to store larger datatype value to smaller datatype
+        //trying to put bucket of water to cup causes dataloss compiler won't do this we have to force compiler to do this
+        int i=128;
+        byte b;
+        //b=i; creates compile time error due to dataloss
+        b=(byte)i;//explicitly typecasting int to byte
+        System.out.println(b);//value printed according to cyclic property
+    }
+}
---
+++ com/ivoyant/week1/primitive_typecasting/ImplicitTypeCasting.java
@@ -0,0 +1,13 @@
+package com.ivoyant.week1.primitive_typecasting;
+
+public class ImplicitTypeCasting {
+    public static void main(String[] args) {
+        //implicit typecasting happens when u try to store smaller datatype to larger datatype
+        //trying to put cup of water to bucket
+        float f;
+        int i;
+        i=10;
+        f=i;//trying to store small datatype value to large datatype
+        System.out.println(f);
+    }
+}
